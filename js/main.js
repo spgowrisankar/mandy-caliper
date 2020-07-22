29f57ca55b7da3e9c917192a3797eb3e0d1dbd19
spgowrisankar <spgowrisankar@gmail.com>
2020-07-22 06:07:39 +0530

    Description Section added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var $ = window.jQuery;
 $(document).ready(function(){
     $('.alert-close-link').on('click', function(){
         $('.alert-bar').fadeOut()
-    })
+    });
     $('.menu-button').on('click', function(){
         var menu =  document.getElementsByClassName(".main-navbar");
         if(menu.style.display === "none") {
@@ -12,5 +12,15 @@ $(document).ready(function(){
         else {
         menu.style.display = "none"
         }
-    })
+    });
+    $('.banner-wrapper').slick({
+        infinite:true,
+        dots:true,
+        arrows:false,
+        rows:0
+    });
+    $('.counter').counterUp({
+    delay: 20,
+    time: 1000
+    });
 })
